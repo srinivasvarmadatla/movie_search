bbc1156534a92fb9932efd3371851c5bb5935241
Srinivas <datlasrinivasvarma@gmail.com>
2024-08-28 23:38:19 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { FavoritesProvider } from './context/favouriteContext.js';
-import {BrowserRouter,Routes,Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 
 
-import Login from "../src/pages/login/Login.jsx";
+
+import Login from './pages/login/Login.jsx';
 import Signup from './pages/signup/Signup.jsx';
 import Search from './pages/search/Search.jsx';
 import Movies from './pages/movies/Movie.jsx';
@@ -14,19 +15,20 @@ import Popular from './pages/popular/Popular.jsx';
 import Favorite from './pages/favourite/Favorites.jsx';
 
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <FavoritesProvider>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
-          <Route path='/' element={<Login />} />
-          <Route path='/sign-up' element={<Signup />} />
-          <Route path='/Search-page' element={<Search />} />
-          <Route path='/Movies' element={<Movies />} />
-          <Route path='/detail' element={<Details />}/>
-          <Route path='/Popular' element={<Popular />}/>
-          <Route path='/favmovies' element={<Favorite />}/>
-          <Route path='/details' element={<Details />}></Route>
+        <Route path='/' element={<Login />} />
+        <Route path='/sign-up' element={<Signup />} />
+        <Route path='/Search-page' element={<Search />} />
+        <Route path='/Movies' element={<Movies />} />
+        <Route path='/detail' element={<Details />} />
+        <Route path='/Popular' element={<Popular />} />
+        <Route path='/favmovies' element={<Favorite />} />
+        <Route path='/details' element={<Details />} />
       </Routes>
     </BrowserRouter>
   </FavoritesProvider>
